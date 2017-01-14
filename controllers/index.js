38590d51378d41ada11f06cd6507882dfd07f68a
Johan64 <johan.peyres@hotmail.fr>
2017-01-14 21:52:42 +0100

    Version 5

--- controllers/index.js
+++ controllers/index.js
@@ -2,6 +2,7 @@ var router = require('express').Router();
 
 router.use('/', require('./homeController'));
 router.use('/post', require('./postController'));
+router.use('/login',require('./loginController'));
 
 module.exports = router;
 
