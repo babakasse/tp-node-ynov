e124509ab88c1b5c05a1259ce45cc150dd43af7f
misterkc007 <babakasse@yahoo.fr>
2017-01-15 23:40:29 +0100

    Version finale provisoire

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -4,4 +4,12 @@ router.get('/', function(req, res) {
   res.render('login.html');
 });
 
+router.post('/', function(req, res) {
+  res.redirect('/index');
+  /*var login = req.body.login;
+  var password = req.body.password;
+  if(login == "admin" && password == "password"){res.redirect('/');}
+  else{console.log("Login ou mot de passe incorrecte");}*/
+});
+
 module.exports = router;
